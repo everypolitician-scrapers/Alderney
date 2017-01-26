b252b46c5f4215b476b5a0b14118ba43ebb1dc6a
Tony Bowden <tony@tmtm.com>
2017-01-26 11:07:03 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -13,7 +14,7 @@ require 'scraped_page_archive/open-uri'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -30,20 +31,20 @@ end
 
 def scrape_mp(url)
   noko = noko_for(url)
-    data = { 
-      id: url[%r{/article/(\d+)/}, 1],
-      name: noko.css('div#contentwrap h1').text.tidy,
-      image: noko.css('div.limage img/@src').text,
-      district: "Alderney",
-      party: "Independent",
-      term: 2014,
-      source: url,
-    }
-    # if matched = noko.css('div#contentwrap').text.match(/Elected to Office in (.*?) (20\d\d)/)
-      # data[:start_date] = Date.parse("%s-%s-01" % matched.captures.reverse).to_s
-    # end
-    data[:image] = URI.join(@BASE, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
-    ScraperWiki.save_sqlite([:id, :term], data)
+  data = {
+    id:       url[%r{/article/(\d+)/}, 1],
+    name:     noko.css('div#contentwrap h1').text.tidy,
+    image:    noko.css('div.limage img/@src').text,
+    district: 'Alderney',
+    party:    'Independent',
+    term:     2014,
+    source:   url,
+  }
+  # if matched = noko.css('div#contentwrap').text.match(/Elected to Office in (.*?) (20\d\d)/)
+  # data[:start_date] = Date.parse("%s-%s-01" % matched.captures.reverse).to_s
+  # end
+  data[:image] = URI.join(@BASE, URI.escape(data[:image])).to_s unless data[:image].to_s.empty?
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 @BASE = 'http://www.alderney.gov.gg'
