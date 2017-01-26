ee2aa3f6c22276785afcc06ed0e7dc1c079186c0
Oliver Denman <oliver@therewillbeanimals.com>
2017-01-26 11:25:00 +0000

    Extract MembersPage

---
+++ lib/members_page.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require 'scraped'
+
+class MembersPage < Scraped::HTML
+  field :member_urls do
+    noko.xpath(
+      './/ul[@id="leftnavigation"]/li[contains(.,"States Members")]/following-sibling::li[@class="child"]//a/@href'
+    )
+        .map(&:text)
+  end
+end
