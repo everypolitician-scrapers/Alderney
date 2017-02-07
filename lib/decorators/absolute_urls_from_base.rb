5e708761b0db4477b00729efac5633ec46eb0b10
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-07 15:03:00 +0000

    Add decorator to make absolute urls from <base> url
    The member pages provide their own base url within the `<base>` tag.
    
    The standard Scraped decorator doesn't know how to handle this and so
    we are rolling our own for this particular scraper.

---
+++ lib/decorators/absolute_urls_from_base.rb
@@ -0,0 +1,33 @@
+require 'nokogiri'
+require 'uri'
+require 'scraped'
+
+class AbsoluteUrlsFromBase < Scraped::Response::Decorator
+  def body
+    Nokogiri::HTML(super).tap do |doc|
+      @doc = doc
+      doc.css('img').each { |img| img[:src] = absolute_url(img[:src]) }
+      doc.css('a').each { |a| a[:href] = absolute_url(a[:href]) }
+    end.to_s
+  end
+
+  private
+
+  attr_reader :doc
+
+  def absolute_url(relative_url)
+    unless relative_url.to_s.empty?
+      URI.join(base_url, URI.encode(
+        # To prevent encoded URLs from being encoded twice
+        URI.decode(relative_url)
+      ).gsub('[', '%5B').gsub(']', '%5D')).to_s
+    end
+  rescue URI::InvalidURIError
+    relative_url
+  end
+
+  def base_url
+    return url if (base = doc.css('base @href').text).empty?
+    base
+  end
+end
