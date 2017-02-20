716ab3f20abd735637b5a413c10eed943b0a3987
Oliver Denman <oliver@therewillbeanimals.com>
2017-02-20 12:16:29 +0000

    Build up data before saving

--- scraper.rb
+++ scraper.rb
@@ -22,10 +22,11 @@ ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 
 url = 'http://www.alderney.gov.gg/article/4077/States-Members'
 term = 2014
-(scrape url => MembersPage).member_urls.each do |mem_url|
-  data = (scrape mem_url => MemberPage).to_h.merge(district: 'Alderney',
-                                                   party:    'Independent',
-                                                   term:     term)
-  # puts data.reject { |k, v| v.to_s.empty? }.sort_by { |k, v| k }.to_h
-  ScraperWiki.save_sqlite(%i(id term), data)
+data = (scrape url => MembersPage).member_urls.map do |mem_url|
+  (scrape mem_url => MemberPage).to_h.merge(district: 'Alderney',
+                                            party:    'Independent',
+                                            term:     term)
 end
+
+# data.each { |d| puts d.reject { |_k, v| v.to_s.empty? }.sort_by { |k, _v| k }.to_h }
+ScraperWiki.save_sqlite(%i(id term), data)
